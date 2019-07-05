96bd4cd28cfe8db5e4854fbb4c78e354a21430f0
RJ Salamanca <salamancaronito@gmail.com>
2019-07-05 19:21:36 -0400

    added a seed sql file

---
+++ sql/seed.sql
@@ -0,0 +1,14 @@
+INSERT INTO users
+VALUES
+    (1, 'Rj', 'Salamanca', 'rj@rj.com', '1');
+
+INSERT INTO routine
+VALUES
+    (1, 'Bench Press, Dumbbell Press, Chest Flies, Tricep Pull Downs');
+
+INSERT INTO exercises
+VALUES
+    (1, 'Bench Press', 1),
+    (2, 'Dumbbell Press', 1),
+    (3, 'Chest Flies', 1),
+    (4, 'Tricep Pull Downs', 1);
